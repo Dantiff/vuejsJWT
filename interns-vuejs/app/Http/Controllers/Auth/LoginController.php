f455fec1831779a56801c2fdd59e9e84f9256c59
Dan <danielmburu674@gmail.com>
2017-02-24 11:09:24 +0300

    setup controller

--- interns-vuejs/app/Http/Controllers/Auth/LoginController.php
+++ interns-vuejs/app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class LoginController extends Controller
 {
@@ -29,11 +33,88 @@ class LoginController extends Controller
 
     /**
      * Create a new controller instance.
-     *
-     * @return void
      */
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+
+    /**
+     * Authenticate the user and return jwt access token
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+
+        $user = $this->validateCredentials($credentials);
+
+        if(!$user instanceof User)
+        {
+            return $user;
+        }
+
+        $token = JWTAuth::attempt($credentials);
+
+        return response()->json(compact('token'));
+
+    }
+
+
+    /**
+     * @param $credentials
+     * @return \Illuminate\Http\JsonResponse
+     */
+    private function validateCredentials($credentials)
+    {
+
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        $user =  User::where('username', $credentials['username'])->first();
+
+        if(!$user->active)
+        {
+            return response()->json('user_inactive', 403);
+        }
+
+        return $user;
+    }
+
+    /**
+     * @return Request
+     */
+    public function getAuthenticatedUser()
+    {
+        try {
+
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+
+        } catch (TokenExpiredException $e) {
+
+            return response()->json(['token_expired'], $e->getStatusCode());
+
+        } catch (TokenInvalidException $e) {
+
+            return response()->json(['token_invalid'], $e->getStatusCode());
+
+        } catch (JWTException $e) {
+
+            return response()->json(['token_absent'], $e->getStatusCode());
+
+        }
+
+        return response()->json($user, 403);
     }
 }
